073200c9f1ed9ab6d418fdc59ecb1f2d889afff8
Huezo <huezo@users.noreply.github.com>
2016-08-20 21:17:55 -0600

    Create huezo.sh

---
+++ huezo.sh
@@ -0,0 +1,30 @@
+#sudo chmod 777  huezo.sh
+
+#  ./huezo.sh
+
+echo Este es un Script sencillo que te dara la version de Ubuntu
+echo Te mostrara el Kernel que usas y si es 32 o 64 bit y otros detalles
+
+echo ""
+
+#lsb_release -a && uname -r   && cat /proc/version
+
+lsb_release -a 
+
+echo 
+
+uname -r   
+
+echo 
+
+cat /proc/version
+
+
+
+echo ""
+echo https://huezohuezo1990.wordpress.com/
+
+#https://huezohuezo1990.wordpress.com/
+
+
+
