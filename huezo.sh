bc8f4a73ad905bf9cb25e31c39691cc9be29689d
Huezo <huezo@users.noreply.github.com>
2016-08-21 02:16:29 -0600

    Update huezo.sh
    Te dire Si es 32 o 64 bit

--- huezo.sh
+++ huezo.sh
@@ -13,18 +13,32 @@ lsb_release -a
 
 echo 
 
-uname -a 
+huezo=$(uname -m)
+if [ "$huezo" = "i686" ]; then
+ echo "Tu Sistema Operativo GNU/Linux es de   32-Bit"
+fi
+if [ "$huezo" = "x86_64" ]; then
+ echo "Tu Sistema Operativo GNU/Linux es de 64-Bit"
+fi
+
+echo 
+
+uname -a  
 
 echo 
 
 cat /proc/version
 
-
+#huezo=$(uname -m)
+#if [ "$huezo" = "i686" ]; then
+# echo "usas  32-Bit"
+#fi
+#if [ "$huezo" = "x86_64" ]; then
+# echo "usas  64-Bit"
+#fi
 
 echo ""
 echo https://huezohuezo1990.wordpress.com/
 
 #https://huezohuezo1990.wordpress.com/
-
-
 
