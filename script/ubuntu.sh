887452134d5520500f0e0bfaae8881aa8a1f5399
Huezo <huezo@users.noreply.github.com>
2016-08-21 02:17:24 -0600

    Te dire Si es 32 o 64 bit
    Te dire Si es 32 o 64 bit

--- script/ubuntu.sh
+++ script/ubuntu.sh
@@ -1,6 +1,6 @@
-#sudo chmod 777  huezo.sh
+#sudo chmod 777  ubuntu.sh
 
-#  ./huezo.sh
+#  ./ubuntu.sh
 
 echo Este es un Script sencillo que te dara la version de Ubuntu
 echo Te mostrara el Kernel que usas y si es 32 o 64 bit y otros detalles
@@ -13,13 +13,29 @@ lsb_release -a
 
 echo 
 
+huezo=$(uname -m)
+if [ "$huezo" = "i686" ]; then
+ echo "Tu Sistema Operativo GNU/Linux es de   32-Bit"
+fi
+if [ "$huezo" = "x86_64" ]; then
+ echo "Tu Sistema Operativo GNU/Linux es de 64-Bit"
+fi
+
+echo 
+
 uname -a  
 
 echo 
 
 cat /proc/version
 
-
+#huezo=$(uname -m)
+#if [ "$huezo" = "i686" ]; then
+# echo "usas  32-Bit"
+#fi
+#if [ "$huezo" = "x86_64" ]; then
+# echo "usas  64-Bit"
+#fi
 
 echo ""
 echo https://huezohuezo1990.wordpress.com/
